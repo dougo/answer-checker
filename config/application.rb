cfc24738753160f30642c5a91f0875ec6d855a11
Doug Orleans <dougorleans@gmail.com>
2021-01-04 17:27:11 -0500

    Add an Application class so that Capybara can start a server and try to visit it.

---
+++ config/application.rb
@@ -0,0 +1,6 @@
+require 'rails/application'
+
+module AnswerChecker
+  class Application < Rails::Application
+  end
+end
--- test/system/check_answer_test.rb
+++ test/system/check_answer_test.rb
@@ -1,9 +1,17 @@
+ENV['RAILS_ENV'] ||= 'test'
+
 require 'action_dispatch/system_test_case'
 require 'webdrivers'
+
+require_relative '../../config/application'
+
+Rails.application.config.eager_load = false
+Rails.application.initialize!
 
 class CheckAnswerTest < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome
 
   test 'check an answer' do
+    visit '/'
   end
 end
